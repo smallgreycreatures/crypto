bceffef86c7f63eb575eba061f8a7ab6393f459a
frno0254 <smallgreycreatures@gmail.com>
2015-03-06 23:48:21 +0100

    Working extended euklides :D

--- src/rsa/RSAEncrypt.java
+++ src/rsa/RSAEncrypt.java
@@ -1,33 +1,58 @@
 package rsa;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Random;
 
 public class RSAEncrypt {
 
 	final int bits = 1024;
 	
-	final BigInteger k = new BigInteger("65537");
+	// has to be 0 < k < m
+	final BigInteger K = new BigInteger("65537");
 	
 	public void init() {
-		BigInteger p, q, n, m,d;
+		BigInteger p, q, n, m = null, d;
 		boolean findingPrimes = true;
+		String filePath = "file.txt";
+
 		//p and q = primes
 		//n = pq
 		//m = (p-1)(q-1)
 		while(findingPrimes) {
-			//Börja med att generera probable primes
+			//find two big primes p and q
 			p		= generateProbablePrime(bits);
 			q		= generateProbablePrime(bits);
+			//n = pq
 			n		= calculateProduct(p,q);
+			//m = (p-1)(q-1)
 			m		= specialPhi(p,q);
 			System.out.println("p = " + p.toString()
-								+ "\nq = " +q.toString());
-			if (gcd(k, m).equals(BigInteger.ONE))
+							+ "\nq = " +q.toString());
+			//check if gcd(k,m) = 1
+			if (gcd(K, m).equals(BigInteger.ONE))
 				findingPrimes = false;
 		}
+		//just testing Works!
+		//BigInteger a = new BigInteger("21");
+		//BigInteger b = new BigInteger("79");
+		//BigInteger c = inverse(a, b);
 		
-
+		//System.out.println(c.toString());
+		d = inverse(K, m);
+		
+		/*try {
+			byte[] file = readFile(filePath);
+			
+			BigInteger message = new BigInteger(file);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+		
 	}
 	
 	private BigInteger generateProbablePrime(int size) {	
@@ -75,12 +100,47 @@ public class RSAEncrypt {
 		return gcdRec(m, k.mod(m));
 	}
 	
+	
+	private byte[] readFile(String filePath) throws IOException {
+		Path path = Paths.get(filePath);
+		byte[] rawData = Files.readAllBytes(path);
+		
+		return rawData;
+	}
+	
+	private BigInteger inverse(BigInteger k, BigInteger m) {
+		BigInteger[] ba = extendedEuclidean(k, m);
+		
+		for (int i = 0; i < ba.length; i++) {
+			
+			System.out.println(ba[i].toString());
+		}
+		if (ba[1].compareTo(BigInteger.ZERO) == 1) {
+			return ba[1];
+		} else {
+			return ba[1].add(m);
+		}
+	}
+	
+	private BigInteger[] extendedEuclidean(BigInteger k, BigInteger m) {
+		
+		if (m.equals(BigInteger.ZERO)) {
+			return new BigInteger[] {k, BigInteger.ONE, BigInteger.ZERO};
+		}
+		
+		BigInteger[] answer = extendedEuclidean(m, k.mod(m));
+		
+		BigInteger x = answer[0];
+		BigInteger y = answer[2];
+		BigInteger temp = calculateProduct(k.divide(m), answer[2]);
+		BigInteger z = answer[1].subtract(temp);
+		
+		return new BigInteger[] {x, y, z}; 
+	}
+	
 	public static void main(String[]args) {
 		RSAEncrypt rsaEncrypt = new RSAEncrypt();
 		rsaEncrypt.init();
 		
-
 	}
-	
-	
 }
